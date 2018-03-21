068ca782ef1aee25a0bf4412b8cee6ee14fa83ad
Philipp Gritsch <csav2278@zid-t9pc1910.uibk.ac.at>
2018-03-21 15:38:20 +0100

    part c

---
+++ fakultaet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+	int a,b;
+	b = 1;
+	int i = 1;
+
+	if(argc == 2){
+	//reading variable	
+	a = atof(argv[1]);
+	if(a == 0){
+		printf("The factorial of %i is %i\n",a,b);
+	} else if(a < 0){
+		printf("The factorial of a negative number is not defined.\n");
+	} else {
+		//while loop for factorial
+		while(i <= a){
+			b = i*b;
+			i++;
+		}
+	printf("The factorial of %i is %i\n",a,b);
+	}
+	} else {
+		printf("Please call the programm with 1 integer value\n");
+	}
+	return 0;
+}
+
+//the computer does calculate the factorial corectly until 13
