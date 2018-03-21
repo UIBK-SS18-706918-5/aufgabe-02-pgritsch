bbf7f81b3bf6bc2bbc6a25f84a78265a5c40d1aa
Philipp Gritsch <csav2278@zid-t9pc1910.uibk.ac.at>
2018-03-21 15:11:53 +0100

    teilaufgabe

--- quader.c
+++ quader.c
@@ -4,20 +4,25 @@
 int main(int argc, char ** argv) {
 	float a, b, c;
 
+	if(argc == 4){
+	//if a and b and c are not empty do calc
+	
 	// Einlesen der Seitenlängen
 	a = atof(argv[1]);
 	b = atof(argv[2]);
 	c = atof(argv[3]);
-
-	// Berechnung der Oberfläche
-	float oberflaeche = 0;
-	oberflaeche += 2.*a*b;
-	oberflaeche += 2.*a*c;
-	oberflaeche += 2.*b*c;
+	
+		// Berechnung der Oberfläche
+		float oberflaeche = 0;
+		oberflaeche += 2.*a*b;
+		oberflaeche += 2.*a*c;
+		oberflaeche += 2.*b*c;
 
 	// Ausgabe
 	printf("Ein Quader mit den Seitenlängen %f, %f und %f hat die Oberfläche %f\n",
 		a, b, c, oberflaeche);
-
+	} else {
+		printf("Please call the programm with 3 float values\n");
+	}
 	return 0;
 }
