0274860a97821b394d704b1eddda5b3b167dfb27
pgritsch <37147033+pgritsch@users.noreply.github.com>
2018-03-23 20:27:23 +0100

    part f v2

---
+++ exp.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+	long double xval;
+	int n =1000;
+	long double xcalc = 1.0;
+	long double factorial=1;
+	long double result=1.0;
+	int i = 1;
+	int j = 1;
+
+	if(argc == 2){
+		xval = atof(argv[1]);
+
+		while(i<=n){
+			//factorial
+			factorial = i * factorial;		
+
+			//powerseries
+			while(j<=i){
+				xcalc = xcalc*xval;
+			j++;
+			}
+		result=result + xcalc/factorial;
+		i++;
+		}
+	printf("exp(%Lf)=%Lf\n",xval,result);
+
+
+	} else {
+		printf("You have to call the function with a float value for x\n");
+	}
+	return 0;
+}
+
